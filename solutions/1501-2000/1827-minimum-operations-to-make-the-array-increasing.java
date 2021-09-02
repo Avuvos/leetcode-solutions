68de759019eb8f7c6cc45063f97a135f10305cd1
Avuvos <avuvos@users.noreply.github.com>
2021-09-02 19:04:42 +0000

    Solved Minimum Operations to Make the Array Increasing (#1827)

---
+++ solutions/1501-2000/1827-minimum-operations-to-make-the-array-increasing.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int cnt = 0;
+        for (int i=0; i<nums.length-1; i++) {
+            if (nums[i] >= nums[i+1]) {
+                cnt += nums[i] - nums[i+1] + 1;
+                nums[i+1] += (nums[i]-nums[i+1] + 1);
+            }
+        }
+        return cnt;
+    }
+}
