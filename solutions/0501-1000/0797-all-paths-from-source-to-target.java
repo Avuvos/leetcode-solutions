4389f9e40a807da081e8da4abeac718297033a1a
Avuvos <avuvos@users.noreply.github.com>
2022-03-05 08:32:03 +0000

    Solved All Paths From Source to Target (#0797)

---
+++ solutions/0501-1000/0797-all-paths-from-source-to-target.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        dfs(graph, 0, path, res);
+        return res;
+    }
+    
+    public void dfs(int[][] graph, int node, List<Integer> path, List<List<Integer>> res) {
+        if (node == graph.length - 1) {
+            res.add(new ArrayList<>(path));         
+            return;
+        }
+        for (int i: graph[node]) {
+            path.add(i);
+            dfs(graph, i, path, res);
+            path.remove(path.size() - 1);
+        }
+    }
+}
