2f7f1f353e48860f389fc002e30f29de5f53f6a0
Avuvos <avuvos@users.noreply.github.com>
2022-04-05 06:21:27 +0000

    Solved Sum of Left Leaves (#0404)

---
+++ solutions/0001-0500/0404-sum-of-left-leaves.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isMyLeftChildLeaf(TreeNode root) {
+        return root.left != null && 
+               root.left.left == null && root.left.right == null;
+    }
+    
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        int res = 0;
+        if (isMyLeftChildLeaf(root)) {
+            res += root.left.val;
+        }
+        return res + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+}
