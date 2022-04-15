9e237cd2ae53b390cdd73bd96fd1dae4dea8e3ec
Avuvos <avuvos@users.noreply.github.com>
2022-04-15 09:38:37 +0000

    Solved Maximum Subarray (#0053)

---
+++ solutions/0001-0500/0053-maximum-subarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr = res = nums[0]
+        for i in range(1, len(nums)):
+            curr = max(curr + nums[i], nums[i])
+            res = max(res, curr)
+        return res
+            
