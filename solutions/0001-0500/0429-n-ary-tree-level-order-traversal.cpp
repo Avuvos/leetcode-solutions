1cd518a1b536852e755e7e747ad438c8743e679b
Avuvos <avuvos@users.noreply.github.com>
2022-09-05 12:51:14 +0000

    Solved N-ary Tree Level Order Traversal (#0429)

---
+++ solutions/0001-0500/0429-n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> res;
+        if (!root) return res;
+        queue<Node*> queue;
+        queue.push(root);
+        while (!(queue.empty())) {
+            int size = queue.size();
+            vector<int> level;
+            for (int i = 0; i < size; ++i) {
+                Node* node = queue.front(); queue.pop();
+                level.push_back(node -> val);
+                for (auto child: node -> children) {
+                    queue.push(child);
+                }
+            }
+            res.push_back(level);
+        }
+        return res;
+    }
+};
