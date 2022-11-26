0350911e94fe139a0e4a49991463c9c38975b3e0
Avuvos <avuvos@users.noreply.github.com>
2022-11-26 14:34:52 +0000

    Solved Minimum Cuts to Divide a Circle (#2481)

---
+++ solutions/2001-2500/2481-minimum-cuts-to-divide-a-circle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfCuts(int n) {
+        if (n == 1) return 0;
+        if (n == 2) return 1;
+        if (n == 3) return 3;
+        if (n == 4) return 2;
+        if (n % 2 == 0) return n / 2;
+        return n;
+        
+        
+    }
+};
