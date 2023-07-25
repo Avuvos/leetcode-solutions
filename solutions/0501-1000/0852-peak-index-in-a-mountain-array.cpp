130abed6cd597662be315178642c5b2773512ecb
Avuvos <avuvos@users.noreply.github.com>
2023-07-25 06:17:52 +0000

    Solved Peak Index in a Mountain Array (#0852)

---
+++ solutions/0501-1000/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = (int) arr.size();
+        int left = 1, right = n - 2;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            } else if (arr[mid] > arr[mid - 1]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
