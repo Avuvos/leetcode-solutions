26b6169da0718b522c6ef43a2a5e177a79f10d81
Avuvos <avuvos@users.noreply.github.com>
2023-07-29 12:34:04 +0000

    Solved Soup Servings (#0808)

---
+++ solutions/0501-1000/0808-soup-servings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def soupServings(self, n: int) -> float:
+        if n >= 4500:
+            return 1
+        
+        @cache
+        def dfs(a, b):
+            if (a <= 0 and b > 0):
+                return 1
+            if a <= 0 and b <= 0:
+                return 0.5
+            if b <= 0:
+                return 0
+            return 1/4 * (dfs(a - 100, b) + dfs(a - 75, b - 25) +
+                          dfs(a - 50, b - 50) + dfs(a - 25, b - 75))
+
+        return dfs(n, n)
