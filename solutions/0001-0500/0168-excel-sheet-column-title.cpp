42ecb08eeea9f92184e3c8308a8a607017f49d8e
Avuvos <avuvos@users.noreply.github.com>
2023-08-22 20:57:33 +0000

    Solved Excel Sheet Column Title (#0168)

---
+++ solutions/0001-0500/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string ans;
+        int n = columnNumber;
+        while (n > 0) {
+            n--;
+            int rem = n % 26;
+            ans += char('A' + rem);
+            n /= 26;
+            
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
