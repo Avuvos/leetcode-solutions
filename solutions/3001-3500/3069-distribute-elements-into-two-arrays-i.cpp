f4f2b0a07eef570b2e328b8684db617dfc269a99
Avuvos <avuvos@users.noreply.github.com>
2024-03-03 07:40:07 +0000

    Solved Distribute Elements Into Two Arrays I (#3069)

---
+++ solutions/3001-3500/3069-distribute-elements-into-two-arrays-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> arr1 = {nums[0]}, arr2 = {nums[1]};
+        for (int i = 2; i < nums.size(); i++) {
+            if (arr1.back() > arr2.back()) {
+                arr1.push_back(nums[i]);
+            } else {
+                arr2.push_back(nums[i]);
+            }
+        }
+        vector<int> result(arr1.begin(), arr1.end());
+        for (auto &x: arr2) result.push_back(x);
+        return result;
+    }
+};
