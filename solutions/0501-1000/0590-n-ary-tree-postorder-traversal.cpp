1b1697fc0f0168ef72775cd46f76d0e35a8af3aa
Avuvos <avuvos@users.noreply.github.com>
2024-08-26 08:04:17 +0000

    Solved N-ary Tree Postorder Traversal (#0590)

---
+++ solutions/0501-1000/0590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> ans;
+        auto dfs = [&](auto &dfs, Node* root) -> void {
+            if (!root) return;
+            for (auto &child: root -> children) {
+                dfs(dfs, child);
+            }
+            ans.push_back(root -> val);
+        };
+        dfs(dfs, root);
+        return ans;
+    }
+};
