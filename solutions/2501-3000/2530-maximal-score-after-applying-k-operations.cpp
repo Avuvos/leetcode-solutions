e8a6c66db58ac8c6dd2e7e78c675d27e4c7e20f5
Avuvos <avuvos@users.noreply.github.com>
2024-10-14 16:41:23 +0000

    Solved Maximal Score After Applying K Operations (#2530)

---
+++ solutions/2501-3000/2530-maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        long long ans = 0;
+        while (pq.top() > 1 && k--) {
+            int x = pq.top(); pq.pop();
+            ans += x;
+            pq.push((x + 2) / 3);
+        }
+        ans += max(0, k);
+        return ans;
+    }
+};
