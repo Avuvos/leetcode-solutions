71d4ba300f23f8e2b6ace12b6668fad56d2052f3
Avuvos <avuvos@users.noreply.github.com>
2026-01-17 12:00:15 +0000

    Solved Count Residue Prefixes (#3803)

---
+++ solutions/3501-4000/3803-count-residue-prefixes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int residuePrefixes(string s) {
+        int n = s.size();
+        unordered_set<char> st;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            st.insert(s[i]);
+            if (st.size() == (i + 1) % 3) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
