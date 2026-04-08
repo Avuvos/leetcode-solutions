fe32232e79f7f355778e85770bfc5028aed4f086
Avuvos <avuvos@users.noreply.github.com>
2026-04-08 11:06:37 +0000

    Solved Integers With Multiple Sum of Two Cubes (#3890)

---
+++ solutions/3501-4000/3890-integers-with-multiple-sum-of-two-cubes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findGoodIntegers(int n) {
+        unordered_map<int, int> mp;
+        for (int a = 1; a <= 1000; a++) {
+            for (int b = a; b <= 1000; b++) {
+                int x = a * a * a + b * b * b;
+                if (x > n) break;
+                mp[x]++;
+            }
+        }
+        vector<int> ans;
+        for (auto [k, v]: mp) {
+            if (v >= 2) {
+                ans.push_back(k);
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
