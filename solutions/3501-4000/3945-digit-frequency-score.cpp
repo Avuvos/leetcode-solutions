170211378a2562588742218b725274f8993a3717
Avuvos <avuvos@users.noreply.github.com>
2026-07-25 09:15:25 +0000

    Solved Digit Frequency Score (#3945)

---
+++ solutions/3501-4000/3945-digit-frequency-score.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int digitFrequencyScore(int n) {
+        vector<int> freq(10);
+        while (n > 0) {
+            freq[n % 10]++;
+            n /= 10;
+        }
+        int ans = 0;
+        for (int d = 0; d < 10; d++) {
+            ans += d * freq[d];
+        }
+        return ans;
+    }
+};
